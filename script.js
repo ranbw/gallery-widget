d9f7b8ac26413aa7811da96e4515f0b4552995e0
Bertrand Wan <203171606+ranbw@users.noreply.github.com>
2026-06-01 21:05:35 -0700

    Add basic functionality

---
+++ script.js
@@ -0,0 +1,49 @@
+const container = document.getElementById("img-container");
+const nextBtn = document.getElementById("next-btn");
+const backBtn = document.getElementById("back-btn");
+
+//Image collection
+const images = ["https://i.pinimg.com/736x/7d/dc/d8/7ddcd8224c0dcdfe09c2a75dd9bb0157.jpg", "https://i.pinimg.com/736x/05/e7/17/05e71716e5a61dded9fa5a84060bc487.jpg", "https://i.pinimg.com/736x/e5/81/13/e58113372e300b799c2bb15c041634b3.jpg"];
+
+//Event listeners
+nextBtn.addEventListener("click", clickNext);
+backBtn.addEventListener("click", clickBack);
+
+//Sets the background to first image in collection
+container.style.backgroundImage = "url('" + images[0] + "')";
+
+let count = 0;
+container.textContent = count + 1;
+
+
+function clickNext() {
+    console.log("Clicked next");
+    count += 1;
+    if (container.style.backgroundImage === null) {
+        return;
+    } else {
+        container.style.backgroundImage = "url('" + images[count] + "')";
+        container.textContent=count + 1;
+        console.log(count);
+    }
+
+}
+
+function clickBack() {
+    console.log("Clicked back")
+    count -= 1;
+    if (count < 0) {
+        return;
+    }
+    container.style.backgroundImage = "url('" + images[count] + "')";
+    container.textContent=count + 1;
+    console.log(count);
+}
+
+
+
+
+
+
+
+
