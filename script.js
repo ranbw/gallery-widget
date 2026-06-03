1860d5bbf47313b138e5522eca34a54eef2d3e95
Bertrand Wan <203171606+ranbw@users.noreply.github.com>
2026-06-02 18:53:31 -0700

    Reposition controls and add select menu

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 const container = document.getElementById("img-container");
 const nextBtn = document.getElementById("next-btn");
 const backBtn = document.getElementById("back-btn");
+const indexDisplay = document.getElementById("index-display");
 
 //Image collection
-const images = ["https://i.pinimg.com/736x/7d/dc/d8/7ddcd8224c0dcdfe09c2a75dd9bb0157.jpg", "https://i.pinimg.com/736x/05/e7/17/05e71716e5a61dded9fa5a84060bc487.jpg", "https://i.pinimg.com/736x/e5/81/13/e58113372e300b799c2bb15c041634b3.jpg"];
+const images = ["https://i.pinimg.com/736x/7d/dc/d8/7ddcd8224c0dcdfe09c2a75dd9bb0157.jpg", "https://i.pinimg.com/736x/05/e7/17/05e71716e5a61dded9fa5a84060bc487.jpg", "https://i.pinimg.com/736x/e5/81/13/e58113372e300b799c2bb15c041634b3.jpg", "https://i.pinimg.com/736x/06/77/58/067758938991e3b0ee501a50607452ec.jpg"];
 
 //Event listeners
 nextBtn.addEventListener("click", clickNext);
@@ -14,7 +15,7 @@ container.style.backgroundImage = "url('" + images[0] + "')";
 
 let index = 0;
 let indexMsg = " of " + images.length;
-container.textContent = index + 1 + indexMsg;
+indexDisplay.textContent = index + 1 + indexMsg;
 
 
 function clickNext() {
@@ -24,7 +25,7 @@ function clickNext() {
     } else {
         index += 1;
         container.style.backgroundImage = "url('" + images[index] + "')";
-        container.textContent = index + 1 + indexMsg;
+        indexDisplay.textContent = index + 1 + indexMsg;
     }
 }
 
@@ -35,7 +36,7 @@ function clickBack() {
     } else {
         index -= 1;
         container.style.backgroundImage = "url('" + images[index] + "')";
-        container.textContent = index + 1 + indexMsg;
+        indexDisplay.textContent = index + 1 + indexMsg;
     }
 }
 
